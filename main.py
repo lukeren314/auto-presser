708abea7d4952b6d91fb072d6de290ee1310366b
lukeren314 <lukeren314@gmail.com>
2021-01-30 11:24:28 -0800

    foundation

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
+import pyautogui
+import time
+
+MAIN_DELAY = 1
+
+switch = False
+on = True
+
+def move_mouse_to(x, y):
+    pass
+
+def click(duration):
+    pass
+
+def hold_click():
+    pass
+
+def release_click():
+    pass
+
+def press_button(key, duration):
+    pass
+
+def hold_button(key):
+    pass
+
+def release_button(key):
+    pass
+
+def wait(time):
+    pass
+
+def doPresses():
+    pass
 
 
 
 if __name__ == "__main__":
-    pass
+    while on:
+        while switch:
+            doPresses()
+            time.sleep(MAIN_DELAY)
